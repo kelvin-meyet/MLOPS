74e0ba96c47946c783d0016456c221e4ed200ee3
Kelvin Ofori-Minta <kelvinmeyet@gmail.com>
2024-04-30 17:49:46 -0400

    project materials uploads

---
+++ Customer_decision_project/run_pipeline.py
@@ -0,0 +1,7 @@
+from pipelines.training_pipeline import train_pipeline
+
+if __name__ == "__main__":
+    # Run Pipeline
+    train_pipeline(
+        data_path="D:/data_science/MLOPS/Customer_decision_project/data/olist_customer_data.csv"
+    )
